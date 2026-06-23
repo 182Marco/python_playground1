9ceb3208dc4f936b76300cc8db8102f18242996d
Filippo Milza <filippo.milzagmail.com@MacBook-Pro.lan>
2026-06-23 15:13:27 +0200

    aggiunti gli streaming

--- 05_streaming.py
+++ 05_streaming.py
@@ -6,29 +6,15 @@ client = OpenAI()
 
 modello = "gpt-4o-mini"
 
-def classifica(testo, client=client):
-    try:
-        risposta = client.responses.create(
-            model= modello,
-            input= testo,
-            instructions="Sei un classifictore di ticket, rispondi con una sola parola tra commerciale, tecnico, fatturazione, spedizione, generale",
-        )
-        return risposta.output_text
-    except AuthenticationError:
-        return "Errore autenticazione"
-    except RateLimitError:
-        return "Troppe richieste" # Qui si potrebbe ad esempio pensare di fargli rifare la chiamata dopo 10s
-    except APIError as e: # questo è il generico, mai metterlo per primo che si mangia gli altri
-        return f"Errore dell'api: {e}"
-    
 
-print(classifica("Non ho ricevuto il collo"))
+risposta = client.responses.create(
+    model= modello,
+    input="Ciao, raccontami la storia degli LLM",
+    stream= True
+)
 
-client = OpenAI(api_key="-wrong-")
+print("streaming: ")
 
-
-print(classifica("Non ho ricevuto il collo", client))
-
-client_robusto = OpenAI(max_retries=5, timeout=20)
-
-print(classifica("Non ho ricevuto il collo", client_robusto))
+for evento in risposta:
+    if evento.type == "response.output_text.delta":
+        print(evento.delta, end="", flush=True) # end è per non andare a capo ogni tocken / flush evita di accumulare cose
